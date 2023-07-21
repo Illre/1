c3d1ff69bc73b4b52ac97f8b624fa236c9140e48
Illre <136290270+Illre@users.noreply.github.com>
2023-07-21 07:42:10 +0100

    Update and rename neww11.js to neww12.js

--- neww12.js
+++ neww12.js
@@ -182,11 +182,11 @@ function run1() {
     });
 
 }
-     const javascript:void = document.querySelector('.addtowish');
+     const void = document.querySelector('.addtowish');
         const notification = document.querySelector('.addtowish');
 
         // إضافة مستمع لنقرة زر "اضف الى السلة"
-        javascript:void addEventListener('click', function () {
+        void addEventListener('click', function () {
             // عرض الإشعار
             notification.style.display = 'block';
 
