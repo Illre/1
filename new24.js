aa1277f066c2e1b62b61fdeef96773bdb4091f64
Illre <136290270+Illre@users.noreply.github.com>
2023-07-21 06:02:57 +0100

    Update new24.js

--- new24.js
+++ new24.js
@@ -47,7 +47,7 @@ var _0x1ecc = function (_0xd5e8de, _0x2530e2) {
         };
      
  setTimeout(function () {
-                addtowish.aktif.display = 'none';
+     addtowish.display = 'none';
             }, 3000); 
        
 
